d844281ca6fe2c4db36d3d1f63656c579c14b28a
Felix <felixomosh7@gmail.com>
2023-12-18 12:12:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        let outMessage = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(outMessage);
+    }
+    return (messages);
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], 'surprise');
+
+
+function countDown (z) {
+    while (z >= 0) {
+        console.log(z);
+        z--
+    }
+    
+}
+
